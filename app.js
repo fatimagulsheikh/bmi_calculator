0717e0f91746a708a495535cfb26aba0560b919b
Fatima Sheikh <162843416+fatimagulsheikh@users.noreply.github.com>
2024-03-15 15:21:14 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+// BMI CALCULATORIN TYPESCRIPT
+var weightInKg = 70;
+var heightInMeters = 1.78;
+var bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log("Fatima Your BMI Is ".concat(bmi));
